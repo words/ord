659c0f4232d667b08a915e367812fb70480ac982
Zeke Sikelianos <zeke@sikelianos.com>
2020-12-09 21:44:38 -0800

    chore: update deps and lint with standard

--- index.js
+++ index.js
@@ -1,56 +1,52 @@
-var express = require('express')
-var logfmt = require('logfmt')
-var merge = require('merge')
-var cors = require('cors')
-var translate = require('wikipedia-translator')
-var wikipedias = require('wikipedias')
-var app = module.exports = express()
+const express = require('express')
+const logfmt = require('logfmt')
+const merge = require('merge')
+const cors = require('cors')
+const translate = require('wikipedia-translator')
+const wikipedias = require('wikipedias')
+const app = module.exports = express()
 
-if (process.env.NODE_ENV !== "test") app.use(logfmt.requestLogger());
-app.use(express.static(__dirname + "/public"))
-app.set('view engine', 'jade');
+if (process.env.NODE_ENV !== 'test') app.use(logfmt.requestLogger())
+app.use(express.static('public'))
+app.set('view engine', 'jade')
 
-app.get('/', cors(), function(req, res) {
-
-  var locals = {
+app.get('/', cors(), function (req, res) {
+  const locals = {
     wikipedias: wikipedias,
     hostname: req.hostname
   }
 
   // Support the old URL format
   if (req.query.query) {
-    var url = require("url").format({
-      pathname: "/search",
-      query:req.query
+    const url = require('url').format({
+      pathname: '/search',
+      query: req.query
     })
     return res.redirect(url)
   }
 
   res.render('index', locals)
+})
 
-});
-
-app.get('/search', cors(), function(req, res) {
-
-  var locals = {
+app.get('/search', cors(), function (req, res) {
+  const locals = {
     wikipedias: wikipedias,
     hostname: req.hostname
   }
 
   // Query param is required
   if (!req.query || !req.query.query) {
-    return res.redirect("/")
+    return res.redirect('/')
   }
 
   // English is the default language
   req.query.lang || (req.query.lang = 'en')
 
   // Log it so we can watch the queries go by
-  if (process.env.NODE_ENV !== "test")
-    logfmt.log(req.query)
+  if (process.env.NODE_ENV !== 'test') { logfmt.log(req.query) }
 
-  translate(req.query.query, req.query.lang, function(err, translation) {
-
+  translate(req.query.query, req.query.lang, function (err, translation) {
+    if (err) console.error(err)
     // "/search?query="+ t.word.toLowerCase() + "&lang=" + t.lang
 
     if (req.query.format && req.query.format.match(/json/)) {
@@ -59,13 +55,11 @@ app.get('/search', cors(), function(req, res) {
       res.render('search', merge(locals, translation))
     }
   })
-
-});
-
+})
 
 if (!module.parent) {
-  var port = process.env.PORT || 5000
-  app.listen(port, function() {
-    console.log("App running on port %s", port)
+  const port = process.env.PORT || 5000
+  app.listen(port, function () {
+    console.log('App running on port %s', port)
   })
 }
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,33 +1,31 @@
 process.env.NODE_ENV = 'test'
 
-var assert = require("assert")
-var supertest = require("supertest")
-var app = require("..")
+// const assert = require('assert')
+const supertest = require('supertest')
+const app = require('..')
 
-describe("GET /", function() {
-
-  it("renders a web view", function(done) {
+describe('GET /', function () {
+  it('renders a web view', function (done) {
     supertest(app)
-      .get("/?query=dog")
+      .get('/?query=dog')
       // .expect("Content-Type", /json/)
       .expect(200)
-      .end(function(err, res) {
+      .end(function (err, res) {
         if (err) return done(err)
         // assert.equal(res.body, request_id)
         return done()
       })
   })
 
-  it("returns JSON if format=json query param is present", function(done) {
+  it('returns JSON if format=json query param is present', function (done) {
     supertest(app)
-      .get("/?query=dog&format=json")
-      .expect("Content-Type", /json/)
+      .get('/?query=dog&format=json')
+      .expect('Content-Type', /json/)
       .expect(200)
-      .end(function(err, res) {
+      .end(function (err, res) {
         if (err) return done(err)
         // assert.equal(res.body, request_id)
         done()
       })
   })
-
 })
